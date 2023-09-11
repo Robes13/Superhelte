1dd89bb3791df7b33eca6a6309a913919d2c27f6
Robert <robertorlanderpedersen@gmail.com>
2023-09-11 13:17:32 +0200

    Add project files.

---
+++ Superhelte/Power.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Superhelte
+{
+    /// <summary>
+    /// Here I have defined that everytime there is a power, 
+    /// there will be a powername, and a description of what that power does.
+    /// </summary>
+    internal class Power
+    {
+        #region Fields
+        private string _powerName;
+        private string _powerDescription;
+        #endregion Fields
+
+        #region Properties
+        public string PowerName
+        { 
+            get { return _powerName; } 
+            set { _powerName = value; } 
+        }
+
+        public string PowerDescription
+        { 
+            get { return _powerDescription; }
+            set { _powerDescription = value; }
+        }
+        #endregion Properties
+    }
+}
---
+++ Superhelte/Superhuman.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Superhelte
+{
+    /// <summary>
+    /// I have defined what variables makes up a superhero, some of the variables I've made to 
+    /// string arrays, because its more suited in some cases. For example, a superhero can have multiple powers,
+    /// gears, suits and be part of more than one superhero group. 
+    /// </summary>
+    internal class Superhuman
+    {
+        #region Fields
+        private string _identity;
+        private Power[] _powers;
+        private string[] _suit;
+        private string[] _gear;
+        private string[] _league;
+        private string _code;
+        private Superhuman _archnemesis;
+        #endregion Fields
+
+        #region Properties
+        public string Identity
+        {
+            get { return _identity; }
+            set { _identity = value; }
+        }
+
+        public Power[] Powers
+        {
+            get { return _powers; }
+            set { _powers = value; }
+        }
+
+        public string[] Suit
+        {
+            get { return _suit; }
+            set { _suit = value; }
+        }
+
+        public string[] Gear
+        {
+            get { return _gear; }
+            set { _gear = value; }
+        }
+
+        public string[] League
+        {
+            get { return _league; }
+            set { _league = value; }
+        }
+
+        public string Code
+        {
+            get { return _code; }
+            set { _code = value; }
+        }
+
+        public Superhuman Archnemesis
+        {
+            get { return _archnemesis; }
+            set { _archnemesis = value; }
+        }
+        #endregion Properties
+    }
+}
